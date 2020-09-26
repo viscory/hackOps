2fc1031ce04325c4ed324d49b06257d15cdfe73b
Faiyaz Rahman <faiyaz_r@outlook.com>
2020-09-26 20:37:28 +0800

    clean_floor

---
+++ codeitsuisse/routes/cluster.py
@@ -0,0 +1,36 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/cluster', methods=['POST'])
+def cluster_finder():
+    data = request.get_json()
+    result = dict()
+    result['answer'] = findingClusters(data)
+
+def dfs(i, j, n, m):
+    area[i][j] = "*"
+    lst = [-1, 0, 1]
+    for i1 in lst:
+        for j1 in lst:
+            if (isValid(i + i1, j + j1, n, m) and area[i + i1][j + j1] != "*"):
+                dfs(i + i1, j + j1, n, m)
+
+def isValid(i, j, n, m):
+    return (i >= 0 and i < n and j >= 0 and j < m)
+               
+def findingClusters(area):
+    count = 0
+    n = len(area)
+    m = len(area[0])
+    for i in range(n):
+        for j in range(m):
+            if(area[i][j] == "1"):
+                dfs(i, j, n, m)
+                count += 1
+    return count
